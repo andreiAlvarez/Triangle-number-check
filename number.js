1b9a862f1dc2ceaa85b5d6907d91df99db15ed77
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-10-11 11:59:47 -0400

    Create number.js

---
+++ number.js
@@ -0,0 +1,20 @@
+// Solution 1
+
+function isTriangleNumber(number) {
+  if (number === 0 || number === 1) {
+    return true;
+  }
+  let c = 0;
+  let newNum = 2 * number;
+  for(let i = 1; i < newNum / 2; i++){
+    if (i * (i + 1) === newNum) {
+      c = 1;
+      break;
+    }
+  }
+
+  return c === 1? true: false; 
+}
+
+// solution 2
+
